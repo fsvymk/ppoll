a610c7423379c1d56fc56988523b35bb303647a5
Mihail <fsvymk@gmail.com>
2016-04-07 20:43:28 +0300

    initial

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <a.out.h>
+#include <unistd.h>
+#include <poll.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    char *buf[32];
+
+    int i;
+    for(i=0;i<32;i++) {
+        buf[i] = 0;
+    }
+
+    int fd = open("/dev/usbtmc0", O_RDWR);
+    printf("fd=%d ",fd);
+
+
+    ssize_t szw;    // size of written in a fact.
+    ssize_t szr;
+
+    int ret;
+
+    while(1){
+
+        szw = write(fd,"READ?\n",6);
+        szr = read(fd, &buf, 32);
+
+        if(szw<0){
+            // fd was closed. re-open it.
+            //printf("reconnect. ");
+            while(!ret){
+                ret = poll(fd, 0, 1000);
+                close(fd);
+                int fd = open("/dev/usbtmc0", O_RDWR);
+            }
+        }
+        else{
+            printf("\nV=%s",buf);
+        }
+    }
+
+    close(fd);
+}
+
+
+
+
+
